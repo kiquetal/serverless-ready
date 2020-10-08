dd4a8d3f213e1275286adf6e278377dce556eae0
kiquetal <kiquetal@gmail.com>
2020-10-07 22:11:44 -0300

    check serverless

--- functions/first-function.js
+++ functions/first-function.js
@@ -1,16 +1,34 @@
 'use strict';
 
-module.exports.main = (event, context, callback) => {
+const co = require("co");
+const Promise = require("bluebird");
+const fs= Promise.promisifyAll(require("fs"));
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const Mustache = require("mustache");
+const URL=process.env.APIG_URL;
+var html;
+
+function* loadHtml() {
+  console.log("here" +JSON.stringify(URL));
+  if(!html)
+    html = yield fs.readFileAsync('static/index.html','utf-8');
+  
+  return html;
+  
+}
+
+module.exports.main = co.wrap( function* (event, context, callback)  {
+  let html = yield loadHtml();
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: html,
+    headers:{
+      'Content-Type':'text/html; charset=UTF-8'
+    }
   };
 
   callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+});
