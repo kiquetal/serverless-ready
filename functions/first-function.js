b683125839cf1985311f2d284fd0209187589009
kiquetal <kiquetal@gmail.com>
2020-10-16 23:45:46 -0300

    Adding new items

--- functions/first-function.js
+++ functions/first-function.js
@@ -3,10 +3,13 @@
 const co = require("co");
 const Promise = require("bluebird");
 const fs= Promise.promisifyAll(require("fs"));
+const aws4 = require ('aws4');
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const Mustache = require("mustache");
-const URL=process.env.APIG_URL;
+const URL = require('url');
+const http = require('superagent-promise')(require('superagent'),Promise);
 var html;
+const restaurantsApiRoot = process.env.restaurants_api;
 
 function* loadHtml() {
   console.log("here" +JSON.stringify(URL));
@@ -16,9 +19,28 @@ function* loadHtml() {
   return html;
   
 }
+function* getRestaurants() {
+  
+  let url = URL.parse(restaurantsApiRoot);
+  let opt ={
+    host:url.hostname,
+    path:url.pathname
+  };
+  aws4.sign(opt);
+  
+  return (yield http.get(restaurantsApiRoot)
+                    .set('Host',opt.headers['Host'])
+                    .set('X-Amz-Date',opt.headers['X-Amz-Date'])
+                    .set('Authorization',opt.headers['Authorization'])
+                    .set('X-Amz-Security-Token',opt.headers['X-Amz-Security-Token'])
+  ).body;
+}
 
 module.exports.main = co.wrap( function* (event, context, callback)  {
-  let html = yield loadHtml();
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template,{ dayOfWeek, restaurants });
   const response = {
     statusCode: 200,
     body: html,
