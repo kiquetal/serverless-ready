9bacb74c3cc943aa0b44549ee2f94f34e006f878
kiquetal <kiquetal@gmail.com>
2020-10-17 01:42:51 -0300

    new page

---
+++ functions/search-restaurants.js
@@ -0,0 +1,32 @@
+'use strict';
+const co = require("co");
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const defaultResults = process.env.defaultResults || 8;
+const tableName = process.env.restaurant_tables;
+
+function* findRestaurantByTheme(count,theme)
+{
+  let req={
+    TableName:tableName,
+    Limit: count,
+    FilterExpression: "contains(themes,:theme)",
+    ExpressionAttributeValues:{":theme":theme}
+  }
+  let resp = yield dynamodb.scan(req).promise();
+  return resp.Items;
+}
+
+module.exports.main = co.wrap(function* (event,context,cb){
+  
+  let restaurants = yield findRestaurantByTheme(defaultResults);
+  let response = {
+    
+    statusCode: 200,
+    body: JSON.stringify(restaurants)
+  }
+  
+  cb(null,response);
+  
+});
